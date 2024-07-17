418356bfc8b882244ccb5df15db1f979a9d72961
muhammadahsanmirza <ahsanmirxa032@gmail.com>
2024-07-17 16:00:21 +0500

    Established Database Connections

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+require('dotenv').config();
+
+// Server Setup
 const express = require('express');
-
 const app = express();
+const PORT = process.env.SERVER_PORT || 5000;
 
-app.listen(3000,()=>{
-    console.log("Server is Running")
-})
+const cors = require('cors');
+
+const connectDB = require('./db/db.connection');
+
+// Connection to database
+connectDB();
+
+
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+
+
+
+//Server Start
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
